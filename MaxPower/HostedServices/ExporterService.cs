c3ee4ce0678d36133975fa40f0cdd47dcf96357b
awaescher <awaescher@gmx.de>
2024-05-19 11:15:31 +0200

    Update metrics names

--- MaxPower/HostedServices/ExporterService.cs
+++ MaxPower/HostedServices/ExporterService.cs
@@ -5,10 +5,10 @@ namespace MaxPower.HostedServices;
 
 public class ExporterService(MaxSettings maxSettings, IEnumerable<InverterConfiguration> inverters, IMaxTalkClient maxTalkClient, ILogger<ExporterService> logger) : BackgroundService
 {
-	private Gauge _energyDay = Metrics.CreateGauge("maxpower_energy_day", "The amount of energy created today in kWh", ["ip", "id"]);
-	private Gauge _energyMonth = Metrics.CreateGauge("maxpower_energy_month", "The amount of energy created this month in kWh", ["ip", "id"]);
-	private Gauge _energyYear = Metrics.CreateGauge("maxpower_energy_year", "The amount of energy created this year in kWh", ["ip", "id"]);
-	private Gauge _energyTotal = Metrics.CreateGauge("maxpower_energy_total", "The amount of energy created in total in kWh", ["ip", "id"]);
+	private Gauge _energyDay = Metrics.CreateGauge("maxpower_energy_day_kwh", "The amount of energy created today in kWh", ["ip", "id"]);
+	private Gauge _energyMonth = Metrics.CreateGauge("maxpower_energy_month_kwh", "The amount of energy created this month in kWh", ["ip", "id"]);
+	private Gauge _energyYear = Metrics.CreateGauge("maxpower_energy_year_kwh", "The amount of energy created this year in kWh", ["ip", "id"]);
+	private Gauge _energyTotal = Metrics.CreateGauge("maxpower_energy_total_kwh", "The amount of energy created in total in kWh", ["ip", "id"]);
 
 	public MaxSettings MaxSettings { get; } = maxSettings ?? throw new ArgumentNullException(nameof(maxSettings));
 	public IEnumerable<InverterConfiguration> Inverters { get; } = inverters ?? throw new ArgumentNullException(nameof(inverters));
